dfd4983508de20205622bfa2ee93c1e19da829eb
Agetedo <114358496+Agetedo@users.noreply.github.com>
2024-06-26 20:18:13 +0300

    Delete DataBase directory

--- DataBase/article02Indicators.js
+++
@@ -1,9 +0,0 @@
-export const article02Indicators = [{
-    id: "art02Ind01",
-    indicator: "30%",
-    caption: "higher salary",
-}, {
-    id: "art02Ind02",
-    indicator: "12,590",
-    caption: "average wage",
-}];
